0178eaecdf21d6bb2f35c33025de04fe826dd7e5
Carlos Eduardo Ribeiro <mail@carlosribeiro.me>
2015-12-13 15:46:07 -0300

    Finish install script

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 # bkp old files
-echo Backuping old files
+echo "Backuping old files"
 mv ~/.vim ~/.vim.old 2> /dev/null
 mv ~/.vimrc ~/.vimrc.old 2> /dev/null
+
+echo "Fetching vim plugins..."
+git submodule init
+git submodule update
+
+echo "Installing dotfiles..."
+cp vimrc ~/.vimrc
+cp -R vim ~/.vim
+
+echo "Instalation finished. enjoy :)"
+
